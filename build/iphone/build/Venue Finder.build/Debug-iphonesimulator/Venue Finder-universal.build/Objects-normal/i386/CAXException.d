b104dc48f4d88ef896e853082dcc7efdd3f1ef0a
Sam Jacob <sam@ksagency.co.uk>
2014-04-02 16:37:00 +0100

    Updating personal build

--- build/iphone/build/Venue Finder.build/Debug-iphonesimulator/Venue Finder-universal.build/Objects-normal/i386/CAXException.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/SamJacob/Desktop/App\ Design/Venue\ Finder/build/iphone/Classes/CAXException.cpp \
-  /Users/SamJacob/Desktop/App\ Design/Venue\ Finder/build/iphone/Classes/CAXException.h \
-  /Users/SamJacob/Desktop/App\ Design/Venue\ Finder/build/iphone/Classes/CADebugMacros.h
--- build/iphone/build/Venue Finder.build/Debug-iphonesimulator/Venue Finder-universal.build/Objects-normal/i386/MGSplitDividerView.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/SamJacob/Desktop/App\ Design/Venue\ Finder/build/iphone/Classes/MGSplitView/MGSplitDividerView.m \
-  /Users/SamJacob/Desktop/App\ Design/Venue\ Finder/build/iphone/Classes/TiBase.h \
-  /Users/SamJacob/Desktop/App\ Design/Venue\ Finder/build/iphone/Classes/TiThreading.h \
-  /Users/SamJacob/Desktop/App\ Design/Venue\ Finder/build/iphone/Classes/TiPublicAPI.h \
-  /Users/SamJacob/Desktop/App\ Design/Venue\ Finder/build/iphone/Classes/MGSplitView/MGSplitDividerView.h \
-  /Users/SamJacob/Desktop/App\ Design/Venue\ Finder/build/iphone/Classes/MGSplitView/MGSplitViewController.h
